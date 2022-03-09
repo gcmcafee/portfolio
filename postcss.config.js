aa097f54bf7e3f99ada1a6f6134c8aabbee161fb
gcmcafee <gcmcafee@gmail.com>
2022-03-09 12:54:51 -0800

    Tooltip Fix

--- postcss.config.js
+++ postcss.config.js
@@ -8,6 +8,7 @@ module.exports = {
                 greedy: [
                     /show/,
                     /collapsing/,
+                    /tooltip/,
                     /navbar-light/,
                     /swiper-slide-active/,
                     /swiper-pagination-clickable/,
