51998fac7e4624350730d591df878bf0479b5954
gcmcafee <gcmcafee@gmail.com>
2022-10-18 14:45:58 -0700

    smarter header

--- web/components/Header.tsx
+++ web/components/Header.tsx
@@ -1,93 +1,159 @@
-import Link from 'next/link';
+import Head from 'next/head';
+import { useState, useEffect } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link, animateScroll as scroll } from 'react-scroll';
 
 export default function Header() {
+  const [expanded, setExpanded] = useState(false);
+
+  // ESC key pressed -> close open full page navigation
+  const detectKeyDown = (e) => {
+    if (e.key === 'Escape') {
+      setExpanded(false);
+    }
+  };
+  useEffect(() => {
+    // Fires: ESC key pressed -> close open full page navigation
+    document.addEventListener('keydown', detectKeyDown, true);
+  }, []);
+
+  const [scroll, setScroll] = useState(false);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      setScroll(window.scrollY > 50);
+    });
+  }, []);
+
   return (
-    <header>
-      <nav
-        id="navbar"
-        className="navbar fixed-top navbar-expand-lg navbar-dark top"
-      >
-        <div className="container-fluid">
-          <div className="navbar-text me-auto">
-            <a href="#hello">
-              UX-Minded
-              <br />
-              Front End Development
-            </a>
+    <>
+      <Head>
+        <title>Cody McAfee | Front End Developer</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="UX-minded front end development with a focus on fluid, lightweight and intuitive web experiences." />
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="favicon.png" />
+        <link rel="icon" sizes="32x32" href="favicon-32x32.png" />
+        <link rel="icon" sizes="57x57" href="favicon-57x57.png" />
+        <link rel="icon" sizes="72x72" href="favicon-72x72.png" />
+      </Head>
+      <a className="skip-to-content" href="#main">
+        Skip to Content
+      </a>
+      <header>
+        <Navbar
+          expanded={expanded}
+          collapseOnSelect
+          expand="lg"
+          variant="dark"
+          id="navbar"
+          fixed="top"
+          className={scroll ? 'not-top' : 'top'}
+        >
+          <div className="container-fluid">
+            <div className="navbar-text me-auto">
+              <Link
+                to="hello"
+                smooth={true}
+                offset={-54}
+                duration={500}
+                href="/"
+              >
+                UX-Minded
+                <br />
+                Front End Development
+              </Link>
+            </div>
+            <Navbar.Toggle
+              aria-controls="navbarTogglee"
+              onClick={() => setExpanded(!expanded)}
+            />
+            <Navbar.Collapse className="text-center" id="navbarTogglee">
+              <div className="overlay" onClick={() => setExpanded(false)}></div>
+              <ul
+                id="nav"
+                className="spy-active navbar-nav mx-auto ms-lg-auto me-lg-0 mb-2 mb-lg-0"
+              >
+                <li className="nav-item">
+                  <span id="active-bar" className="d-none d-lg-block"></span>
+                  <Link
+                    className="nav-link justify-content-center flex-fill"
+                    activeClass="active"
+                    to="hello"
+                    spy={true}
+                    smooth={true}
+                    offset={-54}
+                    duration={500}
+                    onClick={() => setExpanded(false)}
+                    href="#hello"
+                  >
+                    Hello
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link justify-content-center flex-fill"
+                    activeClass="active"
+                    to="work"
+                    spy={true}
+                    smooth={true}
+                    offset={-54}
+                    duration={500}
+                    onClick={() => setExpanded(false)}
+                    href="#work"
+                  >
+                    Work
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link justify-content-center flex-fill"
+                    activeClass="active"
+                    to="about"
+                    spy={true}
+                    smooth={true}
+                    offset={-54}
+                    duration={500}
+                    onClick={() => setExpanded(false)}
+                    href="#about"
+                  >
+                    About
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link justify-content-center flex-fill"
+                    activeClass="active"
+                    to="clients"
+                    spy={true}
+                    smooth={true}
+                    offset={-54}
+                    duration={500}
+                    onClick={() => setExpanded(false)}
+                    href="#clients"
+                  >
+                    Clients
+                  </Link>
+                </li>
+                <li className="nav-item mt-3 mt-lg-0 ms-lg-2">
+                  <Link
+                    className="nav-link navbar-btn btn btn-primary"
+                    activeClass="active"
+                    to="contact"
+                    spy={true}
+                    smooth={true}
+                    offset={-54}
+                    duration={1000}
+                    onClick={() => setExpanded(false)}
+                    href="#contact"
+                  >
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </Navbar.Collapse>
           </div>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarTogglee"
-            aria-controls="navbarTogglee"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className="collapse navbar-collapse text-center"
-            id="navbarTogglee"
-          >
-            <div className="overlay"></div>
-            <ul
-              id="nav"
-              className="spy-active navbar-nav mx-auto ms-md-auto me-md-0 mb-2 mb-md-0"
-            >
-              <li className="nav-item">
-                <span id="active-bar" className="d-none d-md-block"></span>
-                <Link href="/about"
-                  className="nav-link active justify-content-center flex-fill"
-                >
-                  Hello
-                </Link>
-              </li>
-              <li className="nav-item">
-                <span id="active-bar" className="d-none d-md-block"></span>
-                <a
-                  className="nav-link active justify-content-center flex-fill"
-                  href="#hello"
-                >
-                  Hello
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link justify-content-center flex-fill light-navbar"
-                  href="#work"
-                >
-                  Work
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link justify-content-center flex-fill"
-                  href="#about"
-                >
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link justify-content-center flex-fill light-navbar"
-                  href="#clients"
-                >
-                  Clients
-                </a>
-              </li>
-              <li className="nav-item mt-3 mt-md-0 ms-md-2">
-                <a
-                  className="nav-link navbar-btn btn btn-primary"
-                  href="#contact"
-                >
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
-  )
+        </Navbar>
+      </header>
+    </>
+  );
 }
