496612b22ce3313ac090c0f421fb3e0787f05559
gcmcafee <gcmcafee@gmail.com>
2022-10-18 14:45:39 -0700

    breaking out components

---
+++ web/components/Footer.tsx
@@ -0,0 +1,22 @@
+export default function Footer() {
+  
+  return (
+    <footer id="footer" className="text-center">
+        <div className="aside py-1">
+          <div className="container-fluid">
+            <p className="mb-1">
+              * Produced while employed with Teneleven&nbsp;Interactive. ©&nbsp;2022
+              Teneleven&nbsp;Interactive. Third party trademarks are registered
+              trademarks of their respective&nbsp;owners.{' '}
+              <span className="text-nowrap">All Rights Reserved.</span>
+            </p>
+          </div>
+        </div>
+        <div className="main py-3">
+          <div className="container-fluid">
+            <p className="m-0">©&nbsp;2022 Cody McAfee. All Rights&nbsp;Reserved.</p>
+          </div>
+        </div>
+    </footer>
+  );
+}
