94fd8ab08bc813d2bd4ab503bbf97c604a2ff6ea
gcmcafee <gcmcafee@gmail.com>
2023-01-26 09:02:48 -0800

    making date dynamic

--- web/components/Footer.tsx
+++ web/components/Footer.tsx
@@ -1,20 +1,21 @@
 export default function Footer() {
   const isDev = process.env.NODE_ENV === 'development';
+  const year = new Date().getFullYear();
   return (
-    <footer id="footer" className="footer text-center text-sm">
-      <div className="border-t border-gray-300 px-4 py-2 text-slate-800">
+    <footer id="footer" className="text-sm text-center footer">
+      <div className="px-4 py-2 border-t border-gray-300 text-slate-800">
         <p>
-          * Produced while employed with Teneleven Interactive. © 2022 Teneleven
-          Interactive. Third party trademarks are registered trademarks of their
-          respective owners.{' '}
+          * Produced while employed with Teneleven Interactive. © {year}{' '}
+          Teneleven Interactive. Third party trademarks are registered
+          trademarks of their respective owners.{' '}
           <span className="whitespace-nowrap">All Rights Reserved.</span>
         </p>
       </div>
-      <div className="bg-gray-800 px-4 py-4 text-white">
-        <p>© 2022 Cody McAfee. All Rights Reserved.</p>
+      <div className="px-4 py-4 text-white bg-gray-800">
+        <p>© {year} Cody McAfee. All Rights Reserved.</p>
       </div>
       {isDev && (
-        <div className="align-center fixed bottom-3 left-1/2 z-10 hidden -translate-x-1/2 items-center justify-center rounded-full bg-black p-6 text-xs text-white sm:inline-flex">
+        <div className="fixed z-10 items-center justify-center hidden p-6 text-xs text-white -translate-x-1/2 bg-black rounded-full align-center bottom-3 left-1/2 sm:inline-flex">
           <span className="absolute hidden sm:inline md:hidden">
             sm
             <br />
